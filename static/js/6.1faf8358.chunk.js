5f3be5d8cb8732cc171f377ad6506c95998b6926
venusgalstar <venusgalstar@gmail.com>
2022-09-28 19:51:12 +0000

    update frontend

--- static/js/6.1faf8358.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@pancakeswap/interface"]=this["webpackJsonp@pancakeswap/interface"]||[]).push([[6],{672:function(n,c){},673:function(n,c){},682:function(n,c){},683:function(n,c){},703:function(n,c){},706:function(n,c){},707:function(n,c){},712:function(n,c){},716:function(n,c){},766:function(n,c){}}]);
-//# sourceMappingURL=6.1faf8358.chunk.js.map
